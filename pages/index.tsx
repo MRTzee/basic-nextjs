b5bacd8101553031c135033e550c9ccd984417a1
MRTzee <teukumuksalmina280800@gmail.com>
2023-04-11 06:02:41 +0700

    basic-nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/Home.module.css';
+import Layout from '../components/Layout';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <Layout pageTitle="Home Page">
+      <Image src="/profile.jpg" width={200} height={200} alt='profile' />
+      <h1 className={styles['title-homepage']}>Welcome Muksal Mina</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, accusamus ratione impedit necessitatibus animi esse? Ex nulla temporibus accusantium quis saepe dolore illo, autem laborum voluptate vel amet nesciunt ea.</p>
+      <Image src="/profile.jpg" width={200} height={200} alt='profile' />
+    </Layout>
+  )
+}
